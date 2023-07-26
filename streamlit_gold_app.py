462689600b2c39a639d1a279ad09f178c1af7f7b
Zach Freistedt <zfreistedt@gmail.com>
2023-07-25 23:40:32 -0500

    Added requirements file

--- streamlit_gold_app.py
+++ streamlit_gold_app.py
@@ -29,34 +29,7 @@ df = data[['Close']]
 # plt.show()
 
 
-
-
-
-
-
 st.header("Zach's Gold Tracker")
 st.write(f"Current Value: {round(df['Close'][-1], 2)}")
 st.write(f'{date_list[0]} through {date_list[1]}')
 st.line_chart(df)
-
-
-with st.container():
-    st.write("This is in the container")
-
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-
-        st.header("A quarter note")
-        st.image("/Users/zachfreistedt/Documents/projects/streamlit_gold_app/media/quarter_note.png")
-
-    with col2:
-        st.header("A dog")
-        st.image("https://static.streamlit.io/examples/dog.jpg")
-
-    with col3:
-        st.header("An owl")
-        st.image("https://static.streamlit.io/examples/owl.jpg")
-
-
-st.file_uploader("Load here")
